3136ab6f6d2a8676c08baafa3853c97dbdfb5ea3
SoMuchForSubtlety <s0muchfrsubtlety@gmail.com>
2019-02-27 03:15:28 +0100

    multithreading and massive performance improvement

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"sort"
+	"sync"
 
 	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
 )
+
+var app *tview.Application
 
 func main() {
 
@@ -26,60 +29,79 @@ func main() {
 		}
 	}
 
-	//for episode nodes
 	addEpisodes := func(target *tview.TreeNode, parentType int) {
-
-		//create slice of episode IDs and sort it by date
-		var episodes []episodeStruct
-		for _, episode := range vodTypes.Objects[parentType].ContentUrls {
-			episodes = append(episodes, getEpisode(episode))
-		}
-
-		sort.Slice(episodes, func(i, j int) bool {
-			return episodes[i].Title < episodes[j].Title
-		})
-
-		//add them to the tree/ add no content available note
-		if len(episodes) == 0 {
+		//check if episodes of the selected type are not available
+		if len(vodTypes.Objects[parentType].ContentUrls) == 0 {
 			node := tview.NewTreeNode("no content available")
 			node.SetSelectable(false)
 			node.SetColor(tcell.ColorRed)
 			target.AddChild(node)
+			app.Draw()
 		} else {
-			for _, episode := range episodes {
-				node := tview.NewTreeNode(episode.Title).SetSelectable(true)
-				node.SetReference(episode)
+			//store loaded episodes to be sorted at the end
+			var episodes []episodeStruct
+
+			//add waitgroup so sorting doesn't get skipped
+			var wg sync.WaitGroup
+			wg.Add(len(vodTypes.Objects[parentType].ContentUrls))
+
+			//load every episode
+			for i := range vodTypes.Objects[parentType].ContentUrls {
+				//multithread loading the apisodes and add them to the tree dynamically
+				go func(i int) {
+					ep := getEpisode(vodTypes.Objects[parentType].ContentUrls[i])
+					episodes = append(episodes, ep)
+					node := tview.NewTreeNode(ep.Title).SetSelectable(true)
+					node.SetReference(ep)
+					node.SetColor(tcell.ColorGreen)
+					target.AddChild(node)
+					defer wg.Done()
+					app.Draw()
+				}(i)
+			}
+			//wait for loading to complete, then sort
+			wg.Wait()
+			sort.Slice(episodes, func(i, j int) bool {
+				return episodes[i].Title < episodes[j].Title
+			})
+			//purge childrean and re-add them in sorted order
+			target.ClearChildren()
+			for _, ep := range episodes {
+				node := tview.NewTreeNode(ep.Title).SetSelectable(true)
+				node.SetReference(ep)
 				node.SetColor(tcell.ColorGreen)
 				target.AddChild(node)
 			}
+			app.Draw()
 		}
 	}
 
 	//build base tree
 	add(root)
 
+	//what happens when a node is selected
 	tree.SetSelectedFunc(func(node *tview.TreeNode) {
 		reference := node.GetReference()
 		children := node.GetChildren()
 		if reference == nil {
-			return // Selecting the root node does nothing.
+			return //Selecting the root node does nothing.
 		} else if ep, ok := reference.(episodeStruct); ok && len(children) < 1 {
 			downloadAsset(ep.Items[0], ep.Title)
 			newNode := tview.NewTreeNode("content downloaded")
 			node.AddChild(newNode)
 			return
 		}
-		// Load and show files in this directory.
 		if len(children) == 0 {
-			addEpisodes(node, reference.(int))
+			go func() {
+				addEpisodes(node, reference.(int))
+			}()
 		} else {
-			// Collapse if visible, expand if collapsed.
+			//Collapse if visible, expand if collapsed.
 			node.SetExpanded(!node.IsExpanded())
 		}
 	})
 
 	//start UI
-	if err := tview.NewApplication().SetRoot(tree, true).Run(); err != nil {
-		panic(err)
-	}
+	app = tview.NewApplication()
+	app.SetRoot(tree, true).Run()
 }
