1283c2a35e834146b992f3a9f5cb85891fc46eef
SoMuchForSubtlety <s0muchfrsubtlety@gmail.com>
2020-08-08 20:48:03 +0200

    fix: pasting workaround for windows
    adapted from https://github.com/rivo/tview/issues/92
    undo once https://github.com/gdamore/tcell/issues/319 is fixed

--- util.go
+++ util.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"reflect"
 	"regexp"
 	"runtime"
 	"strconv"
 	"strings"
 	"time"
 
+	"github.com/atotto/clipboard"
 	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
 )
@@ -192,4 +194,43 @@ func sanitizeFileName(s string) string {
 	s = whitespace.ReplaceAllString(s, " ")
 	s = strings.TrimSpace(s)
 	return s
+}
+
+// Hacky solution for a working copy paste on Windows
+// Breaks encapsulation by accessing a private member using reflect
+// remove once https://github.com/gdamore/tcell/issues/319 is fixed
+func (session *viewerSession) enablePaste(field *tview.InputField, form *tview.Form) {
+	if runtime.GOOS != "windows" {
+		return
+	}
+	field.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyCtrlV || event.Key() == tcell.KeyCtrlY {
+			session.handlePaste(field)
+		}
+		return event
+	})
+
+	// paste on right click
+	field.SetMouseCapture(func(action tview.MouseAction, event *tcell.EventMouse) (tview.MouseAction, *tcell.EventMouse) {
+		item, _ := form.GetFocusedItemIndex()
+
+		if item != -1 && event.Buttons()&tcell.Button2 != 0 && form.GetFormItem(item) == field {
+			session.handlePaste(field)
+		}
+
+		return action, event
+	})
+}
+
+func (session *viewerSession) handlePaste(field *tview.InputField) {
+	clipContent, err := clipboard.ReadAll()
+	if err != nil {
+		session.logError("could not paste: ", err)
+	}
+	val := reflect.ValueOf(*field)
+	cursorPos := val.FieldByName("cursorPos").Int()
+	text := field.GetText()
+	field.SetText(text[0:cursorPos] + clipContent + text[cursorPos:])
+	// TODO: why does Draw() deadlock?
+	session.app.ForceDraw()
 }
